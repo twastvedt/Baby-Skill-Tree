e6115c084696e5e43f754c231a0db49d11856026
twastvedt <trygve.wastvedt@gmail.com>
2021-08-16 00:28:25 +0000

    Deploying to gh-pages from @ twastvedt/Baby-Skill-Tree@2320053875230b6ea1aedafd8ab3161048a6512d 🚀

--- README.md
+++
@@ -1,4 +0,0 @@
-## Run
-
-1. `npm i`.
-2. `npm start`.
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Graph } from './ts/Graph';
-import { settings } from './ts/settings';
-
-import './scss/index.scss';
-
-(async () => {
-  let graph = new Graph();
-})();
